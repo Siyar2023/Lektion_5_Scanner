af399f298f48d8bef68a7493089856c53e4045c8
Roni7823 <siy_sar78@yahoo.se>
2023-09-19 13:31:28 +0200

    basic scanner Example

--- src/com/siyar/scanner/Main.java
+++ src/com/siyar/scanner/Main.java
@@ -1,7 +1,44 @@
 package com.siyar.scanner;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("input number!");
+            int result = sc.nextInt();
+            System.out.println("You picked: " +result);
+        } while(true);
 
 
+    }
 }
 
+
+
+
+
+//instanstiera: Skapa ett objekt
+//skift + tab: flytta kod till höger.
+//nextLine tar in allting vi gör.
+
+
+
+ /*System.out.println("Please insert a random sentence");
+         sc.nextLine();
+
+         System.out.println("This is unrelated");
+         sc.nextLine(); */
+
+//scanner(); //Kalla på metoden
+
+
+
+/*Do och while är bra om man vill att den ska köras en gång även om det är False.
+do {
+        System.out.println("Did this run once");
+        }
+        while (false); */
